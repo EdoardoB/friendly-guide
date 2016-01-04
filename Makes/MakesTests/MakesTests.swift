b80c7d950e8d2bff984a265ede850385a1f6ccc4
Edoardo Benissimo <edoardo.benissimo@gmail.com>
2016-01-04 15:24:55 +0100

    Create project

---
+++ Makes/MakesTests/MakesTests.swift
@@ -0,0 +1,36 @@
+//
+//  MakesTests.swift
+//  MakesTests
+//
+//  Created by Edoardo Benissimo on 04/01/16.
+//  Copyright © 2016 Motorsquare. All rights reserved.
+//
+
+import XCTest
+@testable import Makes
+
+class MakesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
